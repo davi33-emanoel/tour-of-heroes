482dce9c50c2e115da7481daff3be2ea3dc5afae
davi33-emanoel <emanoeldavi348@gmail.com>
2022-11-09 12:00:41 -0300

    add mock heroes list

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero.model';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +14,16 @@ export class HeroesComponent implements OnInit {
     id:1,
   name:"Wolverine"
   } ;
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Xavier'},
+  {id: 3, name: 'Batman'},
+  {id: 4, name: 'Besta'},
+  {id: 5, name: 'Mulher gato'},
+  {id: 6, name: 'Hulk'},
+  {id: 7, name: 'Homem de ferro'},
+  {id: 8, name: 'Homem aranha'},
+  {id: 9, name: 'Noturno'},
+];
